cb731b023aecfdc297e0ac20f33f161aac646107
KhairunnisaMaharani <khairunnisa.123450071@student.itera.ac.id>
2024-10-12 22:35:26 +0700

    Update halaman_utama.py

--- Halaman Utama/halaman_utama.py
+++ Halaman Utama/halaman_utama.py
@@ -111,7 +111,7 @@ if menu == "Home":
             unsafe_allow_html=True,
         )
         st.write(""" """)
-        foto_kelompok = "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_"
+        foto_kelompok = "https://drive.google.com/uc?export=view&id=1RmnnfCW4rjJs5sZk1qiA3ZgGVC0nSrjl"
         layout(foto_kelompok)
         st.markdown(
             """<div style="text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
